fc47111986017f8817fa0aa4cd8ad7c3892991f9
manuelbadgermaps <manuel@badgermapping.com>
2019-07-19 23:14:53 +0200

    added some code at FileSaver class

--- presenter/FileSaver.py
+++ presenter/FileSaver.py
@@ -1,2 +1,19 @@
+from properties import DATA_FOLDER, FILE_STORAGE_EXTENSION
+
+
 class FileSaver(object):
-    None
+    name = ""
+    __file = ""
+
+    def __init__(self,name):
+        self.name = name
+        self.__file = DATA_FOLDER + name + FILE_STORAGE_EXTENSION
+
+    def __open_file(self):
+        None
+
+    def add_presence(self,time, status):
+        None
+
+    def close_file_saver(self):
+        None
